7cd1ff03d66af4aca9aeaddcf4f90952da5b6b91
dn1123 <112434391+dn1123@users.noreply.github.com>
2022-10-28 08:05:22 +0300

    Add files via upload

---
+++ zad/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите массив строк через пробел");
+string[] symbols = Console.ReadLine()!.Split(' ');
+
+PrintArray(CreatArraySimbols(symbols, 3));
+
+string[] CreatArraySimbols(string[] array1, int numberSymbols)
+{
+    string[] array2 = new string[array1.Length];
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= numberSymbols)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    for (int j = 0; j < result.Length; j++)
+    {
+        result[j] = array2[j];
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine("Сформированный массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
